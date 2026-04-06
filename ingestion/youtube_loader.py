80969e861bc567d2fefd40f91c94c6c68288e15a
amandx36 <xamandeep360@gmail.com>
2026-04-06 11:44:21 +0530

    Added  a function for laoding youtube transcript

---
+++ ingestion/youtube_loader.py
@@ -0,0 +1,15 @@
+# This function is used to load YouTube data 
+from youtube_transcript_api import YouTubeTranscriptApi
+
+def load_youtube_data(video_id, language) -> str:
+    api = YouTubeTranscriptApi()
+    
+    try:
+        transcript_data = api.fetch(video_id, languages=[language])
+        transcript_text = " ".join([entry.text for entry in transcript_data])
+        return transcript_text
+    
+    except Exception as e:
+        print(f"Error fetching transcript for video {video_id}: {e}")
+        return "Error fetching transcript"
+
