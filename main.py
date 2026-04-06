d4f0971098cff5c84c5572f211e57322156d9645
amandx36 <xamandeep360@gmail.com>
2026-04-06 20:44:44 +0530

    Now some bugs are there  i will dix it

--- main.py
+++ main.py
@@ -9,11 +9,10 @@ from processing.chunker import chunk_splitter
 from retrieval.embedder import get_embeddings_from_ollama
 from retrieval.vector_store import create_vector_store
 from retrieval.retriever import fetcher
-
+from utils.formatter import format_docs 
 
 from retrieval.embedder import Embeding_model
-
-from lanchain_core.documents import Document 
+from langchain_core.documents import Document
 
 # pipeline of chat RAG system
 
@@ -27,7 +26,7 @@ def run_pipeline(video_id : str , user_query : str  ):
     #step 3 create the vector store using ollma and chroma and store in chroma db 
 
 
-    chunkList :list[document] = []
+    chunkList : list[Document] = []
     for chunk in chunks:
         chunk_embedding = get_embeddings_from_ollama(chunk)
         print(f"Chunk: {chunk}\nEmbedding: {chunk_embedding}\n")
@@ -37,6 +36,8 @@ def run_pipeline(video_id : str , user_query : str  ):
 
     # step 4 retrival of data using the user query and vector db 
 
+    # step 5 formating the content 
+    content = format_docs(chunks)
     retrieved_chunks = fetcher(vector_store, user_query)
 
     # Step 5  prompt generating 
@@ -48,4 +49,12 @@ def run_pipeline(video_id : str , user_query : str  ):
     response = llm(prompt)
 
     return response
+
+if __name__ == "__main__":
+    video_id = "3T3wMHcrJTuMida5" 
+    user_query = "What is the main topic of the video?"
+    response = run_pipeline(video_id, user_query)
+    print("Response from RAG system:", response)
+
+
 
